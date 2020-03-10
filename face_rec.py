7e23e5fb011baf3a816c523120e9be044b6c78a0
ishaanbilla <ishaanbilla@gmail.com>
2020-03-10 10:34:49 +0530

    Second commit

--- face_rec.py
+++ face_rec.py
@@ -5,28 +5,30 @@ import face_recognition
 import numpy as np
 from time import sleep
 
-
+# get_encoded_faces function encodes all the faces in the specified folder in a dictionary encoded as (name,data)
 def get_encoded_faces():
 
     encoded = {}
 
     for dirpath, dnames, fnames in os.walk("./faces"):
         for f in fnames:
-            if f.endswith(".jpg") or f.endswith(".png"):
+            if f.endswith(".jpg") or f.endswith(".png") or f.endswith(".JPEG"):
                 face = fr.load_image_file("faces/" + f)
                 encoding = fr.face_encodings(face)[0]
                 encoded[f.split(".")[0]] = encoding
 
     return encoded
 
-
+#unknown_image_encoded function encodes the data of the test image given to identify
 def unknown_image_encoded(img):
     face = fr.load_image_file("faces/" + img)
     encoding = fr.face_encodings(face)[0]
 
     return encoding
-
-
+'''
+classify_face function checks the test image with the dataset to mark them if the program recognizes the image 
+and draws a box around them and writes a label below the box
+'''
 def classify_face(im):
 
     faces = get_encoded_faces()
@@ -55,16 +57,17 @@ def classify_face(im):
             cv2.rectangle(img, (left-20, top-20), (right+20, bottom+20), (0, 255, 0), 2)
 
             cv2.rectangle(img, (left-20, bottom -15), (right+20, bottom+20), (0, 255, 0), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
+            font = cv2.FONT_HERSHEY_PLAIN
             cv2.putText(img, name, (left -20, bottom + 15), font, 1.0, (255, 255, 255), 2)
 
-
+    '''
+	Displaying the image with the box and label on the face recognized from the data set
+	'''
     while True:
 
         cv2.imshow('Video', img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             return face_names 
-
 
 print(classify_face("test.jpg"))
 
